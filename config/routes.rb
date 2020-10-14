b04b0bcb181953868633c33f6540b774e9b5cf0e
yamaday0u <35hallelujah00@gmail.com>
2020-10-14 21:46:59 +0900

    update2_20201014

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post "login" => "users#login"
   get "login" => "users#login_form"
 
-  get "/" => "home#top"
+  get "/" => "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
